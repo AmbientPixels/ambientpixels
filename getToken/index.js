f9dd4817e81c650b1048fdefdeb76b7ce45c4aa2
AmbientPixels <ambientpixels2022@gmail.com>
2025-03-26 23:37:24 -0700

    optio s

--- getToken/index.js
+++ getToken/index.js
@@ -1,6 +1,6 @@
 const { v4: uuidv4 } = require('uuid');
 module.exports = async function (context, req) {
-    context.log('getToken called—origin:', req.headers.origin);
+    context.log('getToken called—method:', req.method, 'origin:', req.headers.origin);
     if (req.method === 'OPTIONS') {
         context.res = {
             status: 200,
@@ -8,7 +8,8 @@ module.exports = async function (context, req) {
                 "Access-Control-Allow-Origin": "https://calm-sky-05cc8e110.6.azurestaticapps.net",
                 "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
                 "Access-Control-Allow-Headers": "Content-Type, Authorization"
-            }
+            },
+            body: ""
         };
     } else {
         context.res = {
