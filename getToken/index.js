07fbb6a59e7e0446f3fc1d8007ff757599f35b98
AmbientPixels <ambientpixels2022@gmail.com>
2025-03-26 20:17:18 -0700

    2

--- getToken/index.js
+++ getToken/index.js
@@ -1,9 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
-
 module.exports = async function (context, req) {
-    const token = uuidv4(); // Generates random token, e.g., '550e8400-e29b-41d4-a716-446655440000'
-    context.res = {
-        status: 200,
-        body: { token: token }
-    };
+    const token = uuidv4();
+    context.res = { status: 200, body: { token: token } };
 };
