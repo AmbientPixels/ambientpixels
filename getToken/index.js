ccb5f5dc3eb84a371c6fc3cde0314817a29faa35
AmbientPixels <ambientpixels2022@gmail.com>
2025-03-26 23:11:33 -0700

    TS fixes

--- getToken/index.js
+++ getToken/index.js
@@ -1,5 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
 module.exports = async function (context, req) {
-    const token = uuidv4();
-    context.res = { status: 200, body: { token: token } };
+    context.res = {
+        status: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "https://calm-sky-05cc8e110.6.azurestaticapps.net",
+            "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Authorization"
+        },
+        body: { token: uuidv4() }
+    };
 };
