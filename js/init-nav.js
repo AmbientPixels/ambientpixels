b1904dffb081aa6388fd737b6469e5e0741f4bd8
AmbientPixels <ambientpixels2022@gmail.com>
2025-04-11 08:35:37 -0700

    cleanup(modal): Strip contact modal fetch and use placeholder file

--- js/init-nav.js
+++ js/init-nav.js
@@ -117,10 +117,4 @@ function initFooter() {
       }
     });
 }
-// /js/init-footer.js or init-nav.js
-fetch('/modules/contact-modal.html')
-  .then(res => res.text())
-  .then(html => {
-    document.body.insertAdjacentHTML('beforeend', html);
-    setupContactModal(); // ← defined below
-  });
+
